571042db252a17006619cf5c3a362ebde571c12a
carlhentges <carl.hentges@kcl.ac.uk>
2018-03-01 20:10:43 +0000

    updated ride

--- Ride.h
+++ Ride.h
@@ -7,72 +7,72 @@ using std::abs;
 
 class Ride {
 private:
-    
+
   int identifier;
   Coordinate start;
   Coordinate end;
   int startTime;
   int endTime;
-    
+
     bool compleated;
     bool inPorgress;
-    
-    
+
+
 public:
-    Ride (Coordinate start, Coordinate end, int startTime, int endTime):
-    start(start),end(end),startTime(startTime),endTime(endTime){
+    Ride (Coordinate start, Coordinate end, int startTime, int endTime,int identifier):
+    start(start),end(end),startTime(startTime),endTime(endTime),identifier(identifier){
         compleated = false;
         inPorgress = false;
     }
-    
+
     Coordinate getStart(){
         return start;
     }
-    
+
     Coordinate getEnd(){
         return end;
     }
-    
+
     int getStartTime(){
         return startTime;
     }
     int getEndTime(){
         return endTime;
     }
-    
+
     bool isComplete(){
         return compleated;
     }
-    
+
     void startRide(){
         inPorgress = true;
     }
-    
+
     bool inProgress(){
         return inPorgress;
     }
-    
+
     int getIdentifier(){
       return identifier;
     }
-    
+
     int getSteps(){
-        
+
         int ySteps = 0;
         int xSteps = 0;
-        
+
         ySteps = abs(end.getY() - start.getY());
         xSteps = abs(end.getX() - start.getX());
-        
+
         int totalSteps = 0;
-        
+
         totalSteps = ySteps + xSteps;
-        
+
         return totalSteps;
-        
+
     }
-    
-    
+
+
     ~Ride (){
         //hi
     }
