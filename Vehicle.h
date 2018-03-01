817631b4d1863a48630b0ad2218ab6512c0e89b6
carlhentges <carl.hentges@kcl.ac.uk>
2018-03-01 19:22:18 +0000

    updated vehicle

--- Vehicle.h
+++ Vehicle.h
@@ -4,14 +4,27 @@
 class Vhicle;
 
 #include "Coordinate.h"
-
+#include "Ride.h"
 class Vehicle {
 private:
   Coordinate location;
+  int timeAvalable;
+
 public:
   Vehicle ():location(){
-    location = new coordinate(0,0)
+    location = new coordinate(0,0);
+    timeAvalable = -1;
   }
+
+  bool isAvalable(int timeStamp){
+    return timeStamp > timeAvalable;
+  }
+
+  void move(Ride ride,int currntTime){
+    timeAvalable = currntTime + ride.getSteps();
+    setLoc(ride.getEnd());
+  }
+
   getLoc(){
     return location;
   }
