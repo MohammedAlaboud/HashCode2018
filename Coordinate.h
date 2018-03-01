28f8e545869c2d4032b8250d99e28027a3289669
CarlHentges <carl.hentges@kcl.ac.uk>
2018-03-01 18:04:56 +0000

    new

---
+++ Coordinate.h
@@ -0,0 +1,25 @@
+#ifndef __COORDINATE_H_INCLUDED__
+#define __COORDINATE_H_INCLUDED__
+
+class Coordinate;
+class Coordinate {
+private:
+  int x;
+  int y;
+public:
+  Coordinate (int x, int y):x(x),y(y){
+    //Hi
+  }
+
+  int getX(){
+    return x;
+  }
+  int getY(){
+    return y;
+  }
+
+  ~Coordinate (){
+    //bye
+  }
+};
+#endif
---
+++ Ride.h
@@ -0,0 +1,15 @@
+#ifndef __RIDE_H_INCLUDED__
+#define __RIDE_H_INCLUDED__
+
+class Ride;
+
+class Ride {
+private:
+  /* data */
+
+public:
+  Ride ();
+  ~Ride ();
+};
+
+#endif
