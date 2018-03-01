27f98189ce72098ba5e617bc9ff1bcb297467078
Mohammed Alaboud <mohammed.alaboud97@gmail.com>
2018-03-01 18:57:18 +0000

    implemented closestStart
    closestStart closest ride to the given vehicle

--- Map.h
+++ Map.h
@@ -12,38 +12,65 @@ using std::abs;
 // TODO: your code goes here
 
 class MapOfItems {
-
+    
 private:
-    vector <Ride> rides;
+
     int time = 0;
-
+    vector<Ride> rides;
+    vector<Vehicle> vehicles;
+    
 public:
-
-
-    void addRide(const Ride ride){
-        rides.push_back(ride);
+    
+    vector<Ride> getRides(){
+        return rides;
     }
-
+    vector<Vehicles> getVehicles(){
+        return Vehicles;
+    }
+    
+    void addRide(const Ride r){
+        rides.push_back(r);
+    }
+    
+    void addVehicle(const Vehicle v){
+        vehicles.push_back(v);
+    }
+    
     int steps(Coordinates start, Coordinates end){
-
+        
         int ySteps = 0;
         int xSteps = 0;
-
+        
         ySteps = abs(end.get(y) - start.get(y));
         xSteps = abs(end.get(x) - start.get(x));
-
+        
         int totalSteps = 0;
-
+        
         totalSteps = ySteps + xSteps;
-
+        
         return totalSteps;
     }
-
-
-
-
+    
+    
+    Coordinate closestStart(Vehicle v, rides){
+        
+        closest = rides[0];
+        
+        for(int i = 0; i < rides.size){
+            
+            if(steps(v.getLoc, rides[i].getStart) < closest){
+                
+                closest = rides[i];
+                
+            }
+        }
+        
+        return closest;
+    }
+    
 };
 
 // don't write any code below this line
 
 #endif
+
